e7fa6fb77acb44e2a7c1eb3e58dc87c102f6bf40
Ludovic-Danel <l.danel59@gmail.com>
2024-07-18 19:00:31 +0200

    mise en forme des formulaires

--- src/Form/ArtworkType.php
+++ src/Form/ArtworkType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Artwork;
 use App\Entity\Tag;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,20 +19,14 @@ class ArtworkType extends AbstractType
             ->add('title')
             ->add('description')
             ->add('picture')
-            ->add('createdAt')
-            ->add('drawingCreatedAt')
-            ->add('tag', EntityType::class, [
-                // * c'est un ChoiceType : multiple + expanded
+            ->add('drawingCreatedAt', DateType::class, [
+                'widget' => 'single_text', // Permet d'afficher un calendrier de sélection de date
+                'html5' => true, // Utilise le widget HTML5 si disponible
+            ])            ->add('tag', EntityType::class, [
                 "multiple" => true,
                 "expanded" => true, // radiobutton
-                // ! The required option "class" is missing.
-                // ? à quelle entité est on lié ?
                 "class" => Tag::class,
-                // ! Object of class Proxies\__CG__\App\Entity\Type could not be converted to string
-                // on doit préciser la propriété pour l'affichage
                 'choice_label' => 'name',
-
-
             ])
         ;
     }
